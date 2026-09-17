d251b727a675c14525e6b80e8003bbfa1903913b
Andrés <carlosraxon019@gmail.com>
2026-09-16 19:47:08 -0600

    feat(docs): update sitemap

--- apps/docs/app/sitemap.ts
+++ apps/docs/app/sitemap.ts
@@ -5,9 +5,21 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: SITE.baseUrl,
-      lastModified: new Date('2026-08-27'),
+      lastModified: new Date('2026-09-16'),
       changeFrequency: 'monthly',
       priority: 1,
+    },
+    {
+      url: `${SITE.baseUrl}/docs`,
+      lastModified: new Date('2026-09-16'),
+      changeFrequency: 'monthly',
+      priority: 0.9,
+    },
+    {
+      url: `${SITE.baseUrl}/examples`,
+      lastModified: new Date('2026-09-16'),
+      changeFrequency: 'monthly',
+      priority: 0.9,
     },
   ]
 }
