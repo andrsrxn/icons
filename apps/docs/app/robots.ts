4d99242a93fce9fc4b53ac88c79850435c48924b
Andrés <carlosraxon019@gmail.com>
2026-09-25 12:51:11 -0600

    fix(docs): disallow search query from robots.txt

--- apps/docs/app/robots.ts
+++ apps/docs/app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: ['/terms', '/privacy'],
+      disallow: ['/terms', '/privacy', '/*?q='],
     },
     sitemap: `${SITE.baseUrl}/sitemap.xml`,
   }
